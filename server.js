e4f13e5029e23b29f1dd58c17a1b2a2fd35f802c
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-03 14:08:16 +0500

    express setup

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 console.log("Atta ur Rehman");
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+app.listen(port , () =>
+{
+    console.log(`Server running on port No ${port}`);
+} )
