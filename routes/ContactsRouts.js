2ff925863fe37a6d0c6fc995cebb416a4a9ea7db
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-03 18:15:39 +0500

    Routs

--- routes/ContactsRouts.js
+++ routes/ContactsRouts.js
@@ -9,12 +9,16 @@ router.route("/").post((req,res) =>{
     res.status(200).json({ Message: "Create Contacts"});
 });
 
-router.route("/:id").put((req,res) =>{
-    res.status(200).json({Message: "update Contacts"});
+router.route("/:id").get((req,res) =>{
+    res.status(200).json({Message: `get Contacts${req.params.id}`});
 });
 
-router.route("/:id").get((req,res) =>{
-    res.status(200).send({ Message: "Contacts"});
+router.route("/:id").put((req,res) =>{
+    res.status(200).json({Message: `update Contacts${req.params.id}`});
+});
+
+router.route("/:id").delete((req,res) =>{
+    res.status(200).send({ Message: `delete Contacts${req.params.id}`});
 });
 
 module.exports = router;
