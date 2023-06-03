ddd893d32cba05570d2fe9f90e708d12cd0913b9
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-03 19:19:35 +0500

    error handl with json

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 
 const port = process.env.port || 5000;
 app.use(express.json());
 app.use("/api/contacts", require("./routes/ContactsRouts"));
-
+app.use(errorHandler)
 
 app.listen(port , () =>
 {
