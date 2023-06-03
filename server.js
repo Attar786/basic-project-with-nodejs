c76203fb0f74ca339fed4fe4648918d38905dfe3
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-03 14:48:38 +0500

    api testing one

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-console.log("Atta ur Rehman");
+// console.log("Atta ur Rehman");
 const express = require("express");
-
+const dotenv = require("dotenv").config();
 const app = express();
 
-const port = 5000;
+const port = process.env.port || 5000;
 
+
+app.get("/api/contacts" , (req , res) => {
+    res.send("Get all contscts");
+});
 app.listen(port , () =>
 {
     console.log(`Server running on port No ${port}`);
