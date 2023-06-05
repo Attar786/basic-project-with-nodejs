8891fc3fbd1d67ff118c88a6b2a94047c831fc14
Atta_ur_rehman <attaurr786@outlook.com>
2023-06-05 22:47:44 +0500

    constantsjs done

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,49 @@
-const errorHandler = (err , req, res, next) =>
-{
-    const statusCode = res.statusCode ? res.statusCode : 500;
-    res.json({message: err.message, stackTrack: err.stack})
+const { constants } = require("../constant");
+const errorHandler = (err, req, res, next) => {
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation error",
+        message: err.message,
+        stackTrack: err.stack,
+      });
+
+      break;
+
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not found",
+        message: err.message,
+        stackTrack: err.stack,
+      });
+
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrack: err.stack,
+      });
+
+      break;
+    case constants.UNOTHERIZED:
+      res.json({
+        title: "Not found",
+        message: err.message,
+        stackTrack: err.stack,
+      });
+      case constants.SERVER_ERROR:
+        res.json({
+          title: "Server Error",
+          message: err.message,
+          stackTrack: err.stack,
+        });
+      break;
+    default:
+        console.log("No Error found")
+      break;
+  }
+  const statusCode = res.statusCode ? res.statusCode : 500;
 };
 
 module.exports = errorHandler;
